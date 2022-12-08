ef665eea58f892ed47580d0867d0b4c321ebd67e
Glen Martin <glenem3@gmail.com>
2022-12-07 21:00:50 -0500

    Finish AOC Day 5 puzzle with output file

--- AdventOfCode2022_Py/Advent Of Code Day 5/AOC_Day5.py
+++ AdventOfCode2022_Py/Advent Of Code Day 5/AOC_Day5.py
@@ -16,6 +16,12 @@ stack_list_day5 = [deque("RSLFQ"), deque("NZQGPT"), deque("SMQB"),
 stack_list_Exday5 = [deque("ZN"), deque("MCD"), deque("P")]
 
 
+arr_task2_Day5 = [deque("RSLFQ"), deque("NZQGPT"), deque("SMQB"), 
+                   deque("TGZJHCBQ"), deque("PHMBNFS"), deque("PCQNSLVG"),
+                   deque("WCF"), deque("QHGZWVPM"), deque("GZDLCNR")]
+
+arr_task2_ExDay5 = [deque("ZN"), deque("MCD"), deque("P")]
+
 def OpenText(Data):
     with open(Data, 'r+') as F:
         lines = [line for line in F.readlines()]
@@ -58,26 +64,14 @@ def MakeString(data):
         letters = letters + Example[ex]
     return letters
 
-print("Task 1")
-print('Example')
-print(MakeString(MoveFromStack(stack_list_Exday5, ExDay5)))
-      
-print('Day5 Solution')
-print(MakeString(MoveFromStack(stack_list_day5, Day5)))
+#print(MakeString(MoveFromStack(stack_list_day5, Day5)))
+
+
 '''
 Getting Day 5 task 2 data ready. 
 Altered data structure from a list of stacks to a lists of lists
 Because lists can pop from any index not just the end
 '''
-arr_task2_ExDay5 = []
-for item in [deque("ZN"), deque("MCD"), deque("P")]:
-    arr_task2_ExDay5.append(list(item))
-    
-arr_task2_Day5 = []
-for item in [[deque("RSLFQ"), deque("NZQGPT"), deque("SMQB"), 
-                   deque("TGZJHCBQ"), deque("PHMBNFS"), deque("PCQNSLVG"),
-                   deque("WCF"), deque("QHGZWVPM"), deque("GZDLCNR")]]:
-    arr_task2_Day5.append(list(item))
     
 def MoveFromStack_task2(stacks, instructions):
     #print(stacks)
@@ -89,14 +83,32 @@ def MoveFromStack_task2(stacks, instructions):
         z_re_index = [z[0], z[1]-1, z[2]-1] 
         ts = z_re_index[0]
         
-        if ts == 1:
-            stacks[z_re_index[2]].append(stacks[z_re_index[1]].pop())
-        else:
-            while t < ts:
-                stacks[z_re_index[2]].append(stacks[z_re_index[1]].pop(len(stacks[z_re_index[1]])-ts))
-                t = t + 1
-     #       print(stacks)
-
+        helper_stack = deque()
+        
+        while t < ts:
+            helper_stack.append(stacks[z_re_index[1]].pop())
+            t = t + 1
+        t = 0
+        
+        while t < ts:
+            stacks[z_re_index[2]].append(helper_stack.pop())
+            t = t + 1
+        
     return stacks
 
-print(MakeString(MoveFromStack_task2(stack_list_Exday5, ExDay5)))
+#print(MakeString(MoveFromStack_task2(arr_task2_Day5, Day5)))
+
+
+def new_file(file, day, part1, part2, year=2022):
+    with open(file, "w") as f:
+    
+        f.write("Advent of Code "+str(year)+" Day "+str(day)+" Solutions \n")
+    
+        f.write("Part 1: " + str(part1) + "\n")
+    
+        f.write("Part 2: " + str(part2) + "\n")
+
+new_file("Day 5 Solutions.txt", day=5, 
+         part1=MakeString(MoveFromStack(stack_list_day5, Day5)),
+         part2=MakeString(MoveFromStack_task2(arr_task2_Day5, Day5))
+         )    
